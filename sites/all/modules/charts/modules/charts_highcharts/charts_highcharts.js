6a4012f757a4b4d54bfbfdacdb59350d3f7827d6
Fernando Flores <fflores@avn.info.ve>
2014-07-16 20:41:45 -0430

    cambios al modulo charts

--- sites/all/modules/charts/modules/charts_highcharts/charts_highcharts.js
+++ sites/all/modules/charts/modules/charts_highcharts/charts_highcharts.js
@@ -9,6 +9,24 @@ Drupal.behaviors.chartsHighcharts.attach = function(context, settings) {
   $('.charts-highchart').once('charts-highchart', function() {
     if ($(this).attr('data-chart')) {
       var config = $.parseJSON($(this).attr('data-chart'));
+      Highcharts.setOptions({
+		lang: {
+			months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+			shortMonths: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+		    weekdays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado'],
+		    exportButtonTitle: "Exportar",
+            printButtonTitle: "Imprimir",
+            printChart: "Imprimir gráfico",
+            rangeSelectorFrom: "De",
+            rangeSelectorTo: "Hasta",
+            rangeSelectorZoom: "Periodo",
+            downloadPNG: 'Descargar imagen PNG',
+            downloadJPEG: 'Descargar imagen JPEG',
+            downloadPDF: 'Descargar documento PDF',
+            downloadSVG: 'Descargar imagen SVG',
+            numericSymbols: null,
+            resetZoom: 'Reiniciar zoom',
+		} });
       $(this).highcharts(config);
     }
   });
