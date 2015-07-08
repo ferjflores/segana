6eb966b8c72f63b7743ee3a73fc88bd148e0222a
admin-avn <admin@avn.info.ve>
2015-07-08 16:24:11 -0430

    Cambios del tema

---
+++ sites/all/themes/segana/templates/page-agenda-informativa-preview.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="page">
+
+
+
+  <div id="main">
+
+    <div id="content" class="column" role="main">
+      <?php //print drupal_render(drupal_get_form('informe_agenda_informativa_preview_form', arg(2))) ?>
+    </div>
+
+
+
+  </div>
+
+</div>
+
